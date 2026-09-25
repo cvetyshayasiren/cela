c84b4acb3d96d4d47d1393e0174f8a4cb7afb91d
cvetyshayasiren <75530391+cvetyshayasiren@users.noreply.github.com>
2026-09-25 16:52:40 +0000

    dev

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
+from numpy.ma import in1d
+import curses
 import numpy as np
 
 from main.ca_params import StuckBehaviour
 from randomisation.random_seed import RandomSeed
 
-def main():
-    pass
+def main_test(stdscr):
+    stdscr.refresh()
+    win = curses.newwin(20, 50, 0, 0)
+    win.addstr(2, 2, f"Hello Hello brother\n{'-' * 50}\nwatafak what are you doing?\n{'-' * 50}\nehehe lol kek lal")
+    win.box()
+    win.refresh()
+    stdscr.getch()
+    
 
 if __name__ == "__main__":
-    main()
+    curses.wrapper(main_test)
